a4827c79306a35a6ff3e344871bf06024269c9c2
tavares <140452343+spankaxota@users.noreply.github.com>
2024-10-04 12:25:48 -0300

    dhugb

---
+++ main.js
@@ -0,0 +1,35 @@
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(2,2));
+// let média=function(num1,num2,num3){
+//     return (num1,num2,num3)/num3;
+// }
+// console.log(média(1,2,3));
+
+
+// let num1= parseInt(prompt(8+8+0/3));
+// let num2= parseInt(prompt('Qual n?'));
+// let num3= parseInt(prompt('Qual n?'));
+// document.write(8+8+0/3);
+
+let estados = ['RS','SC','PR','SP','MS','MT','RJ','ES','MG','GO','DF','TO','BA','RO','SE','AL','PE','PB','RN','CE','PI','MA','PA','AP','RR','AM','AC'];
+// console.log(estados);
+// let i = 1
+// while(i < 28){
+//     console.log(i);
+//     console.log(estados[i])
+//     i = i + 1
+// }
+for (let i = 0; i < estados.length; i++) {
+    console.log(i);
+    console.log(estados[i]);
+} 
+let idade = prompt("Qual é tua idade?") ;
+if( 18 > 17){
+console.log('pode dirigir')
+}
+else{
+console.log('não pode dirigir')
+
+}
